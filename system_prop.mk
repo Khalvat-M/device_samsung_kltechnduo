12ae31e3a8ac81fb12b04abd75800a7cc7b11692
Khalvat-M <mmba313@gmail.com>
2023-05-04 18:49:51 +0330

    import klteduos

---
+++ system_prop.mk
@@ -0,0 +1,12 @@
+# Radio
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.multisim.config=dsds \
+    persist.radio.multisim.config=dsds \
+    persist.radio.aosp_usr_pref_sel=true \
+    persist.vendor.radio.add_power_save=1 \
+    persist.vendor.radio.aosp_usr_pref_sel=true \
+    ro.multisim.set_audio_params=true \
+    ro.multisim.simslotcount=2 \
+    ro.qualcomm.perf.cores_online=2 \
+    ro.telephony.ril.config=simactivation \
+    ro.telephony.sim_slots.count=2
